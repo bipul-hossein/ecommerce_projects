91250852623e1347fb4ac05c731224e822af009a
Bipul-hossen <mdbipulhossain252@gmail.com>
2023-09-09 01:44:42 +0600

    working on navbar

--- client/src/components/public/navbar/DesktopView/DesktopView.js
+++ client/src/components/public/navbar/DesktopView/DesktopView.js
@@ -5,20 +5,23 @@ import { CgProfile, CgShoppingCart } from "react-icons/cg";
 const DesktopView = () => {
   return (
     <div>
-      <div>
-        <img
-          src="https://fullkit.moxcreative.com/electrodeals/wp-content/uploads/sites/8/elementor/thumbs/Logo_electrodeals-1-pqh2rs7jj702t6120hf28oa9rgzhabts8ff3d9id4w.png"
-          alt=""
-        />
-        <div>
-          <input type="text" name="" id="" />
-          <button>
-            <GoSearch />{" "}
-          </button>
+      <div className="flex flex-row gap-5 justify-between">
+        <div className="flex flex-row gap-3 w-">
+          <img
+            src="https://fullkit.moxcreative.com/electrodeals/wp-content/uploads/sites/8/elementor/thumbs/Logo_electrodeals-1-pqh2rs7jj702t6120hf28oa9rgzhabts8ff3d9id4w.png"
+            alt=""
+          />
+          <div className="flex w-2/4">
+            <input type="text" name="" id="" className=' border-slate-950 bg-black w-full' />
+            <button className="pl-2">
+              <GoSearch />{" "}
+            </button>
+          </div>
         </div>
-        <div>
-          <div>
-            <div>
+
+        <div className="flex flex-row justify-center gap-5">
+          <div className="flex flex-row justify-center items-center">
+            <div className="text-4xl">
               <CgProfile />
             </div>
             <div>
@@ -26,7 +29,7 @@ const DesktopView = () => {
               <p>Account</p>
             </div>
           </div>
-          <div>
+          <div className="text-4xl">
             <CgShoppingCart />
           </div>
         </div>
