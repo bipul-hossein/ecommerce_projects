23d2668678ee8e8a70f79bdad6886036e4b9f330
Jubayer Ahmed <77119153+jubayer44@users.noreply.github.com>
2023-09-09 02:16:24 +0600

    Update DesktopView.js

--- client/src/components/public/navbar/DesktopView/DesktopView.js
+++ client/src/components/public/navbar/DesktopView/DesktopView.js
@@ -35,8 +35,27 @@ const DesktopView = () => {
               <p>Account</p>
             </div>
           </div>
-          <div>
-            <CgShoppingCart />
+          <div className="relative">
+            <span className=" bg-yellow-400 text-xs text-red-500 rounded-full px-1 absolute -top-2 -right-2 font-bold">
+              1
+            </span>
+            <CgShoppingCart className="text-[28px] text-[#e74c3c]" />
+          </div>
+        </div>
+      </div>
+      <div>
+        {/* Navbar Categories */}
+        <div className="bg-[#e74c3c] px-4 py-6 text-white my-6 flex justify-between items-center">
+          <div className="flex gap-[3%] w-full">
+            <p className="font-bold">Home</p>
+            <p className="font-bold">About</p>
+            <p className="font-bold">Shop</p>
+            <p className="font-bold">Contact Us</p>
+            <p className="font-bold">Services</p>
+          </div>
+          <div className="w-[20%]">
+            <span className="bg-yellow-400 rounded-full p-2 text-xs mr-2 text-red-500 font-bold">%</span>
+            <span className="font-bold">#WeekendDeals</span>
           </div>
         </div>
       </div>
