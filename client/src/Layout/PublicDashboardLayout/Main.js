31b4c1da50ef70a623bf0a2b270214e5767254cb
jubayer44 <jubayerahmedshamim44@gmail.com>
2023-09-10 01:41:20 +0600

    creating-banner-section

--- client/src/Layout/PublicDashboardLayout/Main.js
+++ client/src/Layout/PublicDashboardLayout/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-[1400px] mx-auto">
+      <div className="max-w-[1400px] px-2 mx-auto">
         <Outlet />
       </div>
     </div>
