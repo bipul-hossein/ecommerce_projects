592772fb9cf135353d50c6810005b54a49e68898
jubayer44 <jubayerahmedshamim44@gmail.com>
2023-09-09 18:54:32 +0600

    change-width

--- client/src/Layout/PublicDashboardLayout/Main.js
+++ client/src/Layout/PublicDashboardLayout/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-[1500px] mx-auto">
+      <div className="max-w-[1400px] mx-auto">
         <Outlet />
       </div>
     </div>
