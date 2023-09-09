72d359078ccd4f93bde7248dc114b1515dc0a44b
jubayer44 <jubayerahmedshamim44@gmail.com>
2023-09-09 23:03:34 +0600

    add-search-bar-and-banner-image

--- client/src/components/public/navbar/MobileView/MobileView.js
+++ client/src/components/public/navbar/MobileView/MobileView.js
@@ -1,75 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { AiOutlineHome, AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 import { CgShoppingCart } from "react-icons/cg";
 import { IoPersonOutline } from "react-icons/io5";
-import { Link } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
+const MobileView = ({ activeSearchBar, setActiveSearchBar }) => {
+  const [active, setActive] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-const MobileView = ({activeSearchBar, setActiveSearchBar}) => {
-    const [active, setActive] = useState(0);
-    const [isOpen, setIsOpen] = useState(false);
-   
+  const Menus = [
+    { name: "Menu", icon: <AiOutlineMenu /> },
+    { name: "Search", icon: <AiOutlineSearch /> },
+    { name: "Home", icon: <AiOutlineHome /> },
+    { name: "Cart", icon: <CgShoppingCart /> },
+    { name: "Profile", icon: <IoPersonOutline /> },
+  ];
 
-    const Menus = [
-        { name: "Menu", icon: <AiOutlineMenu />, },
-        { name: "Search", icon: <AiOutlineSearch /> },
-        { name: "Home", icon: <AiOutlineHome /> },
-        { name: "Cart", icon: <CgShoppingCart /> },
-        { name: "Profile", icon: <IoPersonOutline /> },
-    ];
-
-    const handleClick = (i) => {
-        setActive(i)
-        if (i === 0) {
-            setIsOpen(true)
-        }
-
-        if (i === 1) {
-            setActiveSearchBar(!activeSearchBar)
-        }
-
-
-
+  const handleClick = (i) => {
+    setActive(i);
+    if (i === 0) {
+      setIsOpen(true);
     }
 
-    console.log(activeSearchBar)
+    if (i === 1) {
+      setActiveSearchBar(!activeSearchBar);
+    }
+  };
 
-    return (
-        <>
-            <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-            <div className="bg-[#e7e7e7] max-h-[4.4rem] w-full">
-                <ul className="flex relative">
-
-                    {Menus.map((menu, i) => {
-
-                        return <li key={i} className="w-full">
-                            <Link href='/' className={`flex flex-col justify-center items-center text-center ${i === active
-                                ? 'text-white bg-[#e74c3c] pt-2'
-                                : 'pt-3 flex-none'
-                                }`}
-                                onClick={() => handleClick(i)}
-                            >
-                                <span
-                                    className={`text-xl cursor-pointer ${i === active && 'text-white'}`}
-                                >
-                                    {menu.icon}
-                                </span>
-                                <span
-                                    className={`text-sm ${i === active
-                                        ? "translate-x-6-4 opacity-100 "
-                                        : "hidden opacity-0 translate-y-10 "
-                                        }  `}
-                                >
-                                    {menu.name}
-                                </span>
-                            </Link>
-                        </li>
-                    })}
-                </ul>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div className="bg-[#e7e7e7] max-h-[4.4rem] w-full">
+        <ul className="flex relative">
+          {Menus.map((menu, i) => {
+            return (
+              <li key={i} className="w-full">
+                <Link
+                  href="/"
+                  className={`flex flex-col justify-center items-center text-center ${
+                    i === active
+                      ? "text-white bg-[#e74c3c] pt-2"
+                      : "pt-3 flex-none"
+                  }`}
+                  onClick={() => handleClick(i)}
+                >
+                  <span
+                    className={`text-xl cursor-pointer ${
+                      i === active && "text-white"
+                    }`}
+                  >
+                    {menu.icon}
+                  </span>
+                  <span
+                    className={`text-sm ${
+                      i === active
+                        ? "translate-x-6-4 opacity-100 "
+                        : "hidden opacity-0 translate-y-10 "
+                    }  `}
+                  >
+                    {menu.name}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
 
-export default MobileView;  
+export default MobileView;
--- client/src/components/public/navbar/Navbar.js
+++ client/src/components/public/navbar/Navbar.js
@@ -6,18 +6,33 @@ import SearchBar from "./MobileView/SearchBar";
 const Navbar = () => {
   const [activeSearchBar, setActiveSearchBar] = useState(false);
 
-
   return (
     <>
       <div className="hidden md:block">
         <DesktopView />
       </div>
-      <div>
-        <div className={`${activeSearchBar ? 'block' : 'hidden'} bg-white py-3`}>
+
+      {/* Mobile View */}
+      <div className="md:hidden">
+        <div className="border-b border-[1px] border-gray-400 py-1 fixed top-0 z-10 bg-white w-full">
+          <img
+            className="mx-auto"
+            src="https://fullkit.moxcreative.com/electrodeals/wp-content/uploads/sites/8/elementor/thumbs/Logo_electrodeals-1-pqh2rs7jj702t6120hf28oa9rgzhabts8ff3d9id4w.png"
+            alt=""
+          />
+        </div>
+        <div
+          className={`${
+            activeSearchBar ? "block" : "hidden"
+          } bg-white py-3 fixed w-full z-10 border-b border-[1px] border-gray-400`}
+        >
           <SearchBar />
         </div>
-        <div className="fixed bottom-0 z-10 w-full md:hidden">
-          <MobileView activeSearchBar={activeSearchBar} setActiveSearchBar={setActiveSearchBar} />
+        <div className="fixed bottom-0 z-10 w-full ">
+          <MobileView
+            activeSearchBar={activeSearchBar}
+            setActiveSearchBar={setActiveSearchBar}
+          />
         </div>
       </div>
     </>
