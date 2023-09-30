b669b2e22e47d78eb7a86de09cc51a507b5e29fc
Bipul-hossen <mdbipulhossain252@gmail.com>
2023-09-30 13:43:31 +0600

    handle category frontend added

--- client/src/hooks/useMainCategories.js
+++ client/src/hooks/useMainCategories.js
@@ -5,6 +5,7 @@ const useMainCategories = () => {
 
     useEffect(()=> {
         fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        //fetch('http://localhost:5000/categories')
         .then(res => res.json())
         .then(data => {
           setMainCategories(data?.categories)
--- server/controllers/categoriesController.js
+++ server/controllers/categoriesController.js
@@ -1,11 +1,13 @@
 const Category = require("../models/categoryModel");
 const { successResponse } = require("./responseController");
-const slugify = require('slugify')
+const slugify = require('slugify');
+const createError = require("http-errors")
 // create category function
 const handleCreateCategories = async (req, res, next) => {
     try {
         // step 1: get the data from request
         const { title } = req.body;
+        console.log(title)
         // step 2: save to database with create function
         const newCategory = await Category.create({
             title: title,
@@ -41,7 +43,7 @@ const handleGetCategories = async (req, res, next) => {
 const handleGetCategory = async (req, res, next) => {
     try {
         const { slug } = req.params;
-        const getCategory = await Category.findOne({slug}).select('title slug').lean()
+        const getCategory = await Category.findOne({ slug }).select('title slug').lean()
 
         return successResponse(res, {
             statusCode: 200,
@@ -53,5 +55,54 @@ const handleGetCategory = async (req, res, next) => {
     }
 };
 
+// Update a single category function
+const handleUpdateCategory = async (req, res, next) => {
+    try {
+        const {title} = req.body;
+        const {slug} = req.params;
+        const filter = { slug };
+        const updates = { $set: { title: title, slug: slugify(title) } }
+        const option = {
+            new: true
+        }
+        const updateCategory = await Category.findOneAndUpdate(filter, updates, option);
+
+        if(!updateCategory){
+            throw createError(404,'Category not found')
+        }
+        return successResponse(res, {
+            statusCode: 201,
+            message: "Category updated successfully",
+            payload: updateCategory,
+        })
+    } catch (error) {
+        next(error)
+    }
+};
+
+// Delete a single category function
+const handleDeleteCategory = async (req, res, next) => {
+    try {
+        const {slug} = req.params;
+        const deleteCategory = await Category.findOneAndDelete({slug});
+        if(!deleteCategory){
+            throw createError(404,'Category not found')
+        }
+        return successResponse(res, {
+            statusCode: 201,
+            message: "Category Deleted successfully",
+            payload: deleteCategory,
+        })
+    } catch (error) {
+        next(error)
+    }
+};
+
 // exports handleCreateCategory to reuse from category routes
-module.exports = { handleCreateCategories, handleGetCategories, handleGetCategory }; 
+module.exports = {
+    handleCreateCategories,
+    handleGetCategories,
+    handleGetCategory,
+    handleUpdateCategory,
+    handleDeleteCategory
+}; 
