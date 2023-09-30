e5a6e8c509d329c7a4a26cda7a4763ef44fc05f0
jubayer44 <jubayerahmedshamim44@gmail.com>
2023-09-30 15:55:26 +0600

    changes

--- server/index.js
+++ server/index.js
@@ -1,70 +1,34 @@
 const express = require("express");
-const mongoose = require('mongoose'); //set.1 require mongoose
-const cors = require("cors");//middleware
+const mongoose = require("mongoose");
+const cors = require("cors");
 const categoriesRouter = require("./routes/categoriesRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-
-//data type use middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// set.3 create a schema
-const dataSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
-});
-
-//set.4 create a schema model
-const Product = mongoose.model('products', dataSchema);
 
 // set.2 connect to DataBase
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/e-bazar');
-
+    await mongoose.connect("mongodb://127.0.0.1:27017/e-bazar");
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-
-    console.log("Database is connected")
+    console.log("Database is connected");
   } catch (error) {
     console.log("Database is not connected");
     console.log(error);
   }
-}
+};
 
-app.post("/product", async (req, res) => {
-  try {
-    const newProduct = new Product({
-      title: req.body.title,
-      price: req.body.price,
-    })
-    const productData = await newProduct.save()
-    res.status(201).send(productData);
-  } catch (error) {
-    res.status(500).send({ message: error.message })
-  }
+app.use("/categories", categoriesRouter);
+
+app.get("/", (req, res) => {
+  res.send("welcome to home page");
 });
 
- app.use('/categories',categoriesRouter);
-
- //end code
- app.get("/", (req, res) => {
-   res.send("welcome to home page");
-  });
-
-  app.listen(port, async () => {
-    console.log(`server is running at http://localhost:${port}`); await connectDB();
-  });
+app.listen(port, async () => {
+  console.log(`server is running at http://localhost:${port}`);
+  await connectDB();
+});
