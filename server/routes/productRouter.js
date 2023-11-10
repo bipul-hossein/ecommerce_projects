80e224e1bd7f761c32914c399e68fc4a50f4baa8
Bipul-hossen <mdbipulhossain252@gmail.com>
2023-11-10 22:17:30 +0600

    error-

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -14,10 +14,10 @@ const productRouter = express.Router();
 // Image upload with multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-   cb(null, "uploads/");
+  return cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-   cb(
+  return cb(
       null,
       file.fieldname + "_" + Date.now() + "_" + file.originalname
     );
