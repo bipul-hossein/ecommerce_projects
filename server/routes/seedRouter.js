8014cbb86031e5420535c7c31e1f73565a88dd00
Bipul-hossen <mdbipulhossain252@gmail.com>
2023-11-13 20:08:12 +0600

    add faq

--- server/routes/seedRouter.js
+++ server/routes/seedRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { seedProducts } = require('../controllers/seedController');
+const { seedProducts, seedFaq } = require('../controllers/seedController');
 const seedRouter = express.Router();
 
 seedRouter.get("/seed/products", seedProducts);
+seedRouter.get("/seed/faq", seedFaq);
 
 module.exports = seedRouter;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { handleCreateUser, handleGetUser, handleUpdateUser, handleCreateAddress, handleGetAddress, handleGetAllUser,handleJWT,verifyJWT } = require('../controllers/userController');
+const { handleCreateUser, handleGetUser, handleUpdateUser, handleCreateAddress, handleGetAddress, handleGetAllUser,handleJWT,verifyJWT, handleCreateFaq, handleAllGetFaq, handleGetFaq, handleUpdateFaq, handleDeleteFaq } = require('../controllers/userController');
 //const { handleGetUserOrders } = require('../controllers/ordersController');
 const userRouter = express.Router();
 
@@ -16,5 +16,12 @@ userRouter.put('/user', handleUpdateUser);
 userRouter.put('/user/address', handleCreateAddress);
 
 userRouter.get('/user/address',verifyJWT, handleGetAddress);
+
+//faq
+userRouter.post('/faq',handleCreateFaq);
+userRouter.get('/faqs',handleAllGetFaq);
+userRouter.get('/faq/:id',handleGetFaq);
+userRouter.put('/faq/:id',handleUpdateFaq);
+userRouter.delete('/faq/:id',handleDeleteFaq);
 
 module.exports = userRouter;
